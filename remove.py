b42ac1ba33cf35cd228b5c6bd1aba9188045ccdd
Müşerref Selçuk Özdemir <72154925+SelcukOzdemir23@users.noreply.github.com>
2023-07-07 15:03:29 +0300

    değisiklik8

--- remove.py
+++ remove.py
@@ -4,14 +4,13 @@ from PIL import Image
 import base64
 
 def main():
-    # Özel logo ekleme
-    logo_image = Image.open("unnamed.png")
-    st.image(logo_image, width=200)
     st.title("Arka Plan Kaldırıcı")
 
     uploaded_file = st.file_uploader("Lütfen bir fotoğraf yükleyin", type=["jpg", "jpeg", "png"])
 
-  
+    # Özel logo ekleme
+    logo_image = Image.open("path/to/your/logo.png")
+    st.image(logo_image, width=200)
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -25,25 +24,17 @@ def main():
                     output = remove(image)
                     st.image(output, caption="Arka Planı Yok Edilmiş Fotoğraf", use_column_width=True)
 
-                    save_button = st.button("Fotoğrafı İndir")
-                    if save_button:
-                        save_image(output)
+                    if st.button("Fotoğrafı İndir"):
+                        download_image(output)
 
 
-def save_image(output):
+def download_image(output):
     # Çıktıyı geçici bir dosyaya kaydedin
     output_path = "output.png"  # Kaydedilecek dosya adı ve uzantısı
     output.save(output_path, "PNG")  # Çıktıyı PNG formatında kaydet
 
-    # Dosyayı okuyun ve base64 kodlayın
-    with open(output_path, "rb") as file:
-        encoded_image = base64.b64encode(file.read()).decode()
-
-    # İndirme bağlantısını oluşturun
-    href = f'<a href="data:file/png;base64,{encoded_image}" download="output.png">Fotoğrafı İndir</a>'
-
-    # İndirme bağlantısını görüntüleyin
-    st.markdown(href, unsafe_allow_html=True)
+    # Dosyayı indirme butonuyla indirin
+    st.download_button(label="Fotoğrafı İndir", data=output_path, file_name="output.png")
 
 
 if __name__ == "__main__":
